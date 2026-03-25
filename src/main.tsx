a61dc23886e3e2b0204f6c3b60ca44a0fd72db4d
Dmytro <nova_dima@ukr.net>
2026-03-26 01:46:50 +0200

    add all tsx and css

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './components/App.tsx';
 
-createRoot(document.getElementById('root')!).render(
+import App from './components/App/App.tsx';
+// Нормалізація стилів
+import 'modern-normalize';
+
+// import './index.css';
+
+createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <App />
   </StrictMode>
